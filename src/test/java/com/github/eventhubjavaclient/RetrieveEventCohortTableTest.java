79517606cbd51b8041b671d03c36c3e87caf6270
tom-ogle <t.ogle.uk@gmail.com>
2014-12-10 21:22:46 +0000

    Temporarily ignored placeholder test classes for CI build

--- src/test/java/com/github/eventhubjavaclient/RetrieveEventCohortTableTest.java
+++ src/test/java/com/github/eventhubjavaclient/RetrieveEventCohortTableTest.java
@@ -1,8 +1,10 @@
 package com.github.eventhubjavaclient;
 
 import mockit.integration.junit4.JMockit;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
+@Ignore
 @RunWith(JMockit.class)
 public class RetrieveEventCohortTableTest extends EventHubClientTestBase {
   // TODO: Unit tests
